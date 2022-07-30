c6ae6b5a35ea7dcc48dc7a0786556c1d86a625e5
kanesmall <kanesmall@outlook.com>
2022-07-30 22:59:15 +0100

    build: responsive testing for tabcards component

--- src/components/TabCards/index.tsx
+++ src/components/TabCards/index.tsx
@@ -9,28 +9,32 @@ interface ITabCardsProps {
 }
 
 export const TabCards: React.FC<ITabCardsProps> = ({ title, tabs }) => (
-    <div className="xl:container mx-auto">
-        <h2 className="font-sans text-white font-semibold text-center text-5xl max-w-4xl mx-auto mb-8-">{title}</h2>
+    <div className="xl:container mx-auto py-36">
+        <h2 className="font-sans text-white font-semibold text-center text-5xl max-w-4xl mx-auto mb-8">{title}</h2>
         {tabs &&
             tabs.map((tab, index) => (
                 <>
                     <button className="font-sans text-white" key={index}>
                         {tab.label}
                     </button>
-                    <div className="grid grid-cols-2 gap-7 max-w-4xl mx-auto">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-7 justify-items-center max-w-4xl mx-auto">
                         {tab.cards &&
                             tab.cards.map((card, index) => (
-                                <div key={index} className="flex items-center bg-[#393939] p-5 rounded-3xl">
+                                <div key={index} className="flex items-center max-w-[410px] md:max-w-none w-full">
                                     <ConditionalWrapper
                                         condition={!!card.link}
                                         wrapper={(children) => (
-                                            <a className="flex items-center h-full w-full" href={card.link}>
+                                            <a
+                                                className="flex items-center h-full w-full bg-[#393939] p-4 sm:p-5 rounded-3xl lg:hover:-translate-y-2 transition hover:ease-in duration-200"
+                                                href={card.link}
+                                                target="_blank"
+                                            >
                                                 {children}
                                             </a>
                                         )}
                                     >
                                         {card.logo && (
-                                            <div className="relative max-h-[38px] max-w-[38px] h-full w-full mr-7">
+                                            <div className="relative max-h-[28px] sm:max-h-[38px] max-w-[28px] sm:max-w-[38px] h-full w-full mr-4 sm:mr-7">
                                                 <Image
                                                     src={`/logos/${card.logo}`}
                                                     alt={`${card.title} logo`}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
-import "../../styles/globals.css";
-import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import '../../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
     useEffect(() => {
-        document.body.classList.add("bg-[#292929]");
-    });
+        document.body.classList.add('bg-[#292929]')
+    })
 
     return (
-        <div className="px-8">
+        <div className="px-7">
             <Component {...pageProps} />
         </div>
-    );
+    )
 }
 
-export default MyApp;
+export default MyApp
