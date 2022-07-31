08e308df34d2031c09d10d50c054c64ca53e25a5
kanesmall <kanesmall@outlook.com>
2022-07-31 16:22:36 +0100

    build: implemented tabbing functionality

--- src/components/TabCards/TabsData.ts
+++ src/components/TabCards/TabsData.ts
@@ -91,4 +91,118 @@ export const tabsData: Tabs[] = [
             },
         ],
     },
+    {
+        icon: '',
+        label: 'Tools',
+        cards: [
+            {
+                logo: 'vscode-logo.svg',
+                title: 'VS Code',
+                desc: 'The only code editor you should be using',
+                link: 'https://code.visualstudio.com/',
+            },
+            {
+                logo: 'github-logo.svg',
+                title: 'GitHub',
+                desc: 'Version control and source code management host',
+                link: 'https://github.com/',
+            },
+            {
+                logo: 'slack-logo.svg',
+                title: 'Slack',
+                desc: 'Work-based messaging platform',
+                link: 'https://slack.com/',
+            },
+            {
+                logo: 'lambdatest-logo.svg',
+                title: 'Lambddatest',
+                desc: 'Cross-browsing testing tool',
+                link: 'https://www.lambdatest.com/',
+            },
+            {
+                logo: 'ziflow-logo.svg',
+                title: 'Ziflow',
+                desc: 'Review and approval platform',
+                link: 'https://www.ziflow.com/',
+            },
+            {
+                logo: 'jira-logo.svg',
+                title: 'Jira',
+                desc: 'Project planning and tracking tool',
+                link: 'https://www.atlassian.com/software/jira',
+            },
+            {
+                logo: 'loom-logo.svg',
+                title: 'Loom',
+                desc: 'Desktop recording software',
+                link: 'https://www.loom.com/',
+            },
+            {
+                logo: 'figma-logo.svg',
+                title: 'Figma',
+                desc: 'Collaborative design tool',
+                link: 'https://www.figma.com/',
+            },
+        ],
+    },
+    {
+        icon: '',
+        label: 'Apps',
+        cards: [
+            {
+                logo: 'lastpass-logo.svg',
+                title: 'LastPass',
+                desc: 'Password manager',
+                link: 'https://www.lastpass.com/',
+            },
+            {
+                logo: 'lastpass-authenticator-logo.svg',
+                title: 'LastPass Authenticator',
+                desc: '2FA authenticator',
+                link: 'https://www.lastpass.com/solutions/authentication',
+            },
+            {
+                logo: 'spotify-logo.svg',
+                title: 'Spotify',
+                desc: 'Audio streaming service',
+                link: 'https://www.spotify.com/uk/',
+            },
+            {
+                logo: 'monzo-logo.svg',
+                title: 'Monzo',
+                desc: 'App-based banking',
+                link: 'https://monzo.com/',
+            },
+            {
+                logo: 'notion-logo.svg',
+                title: 'Notion',
+                desc: 'Note-taking software',
+                link: 'https://www.notion.so/',
+            },
+            {
+                logo: 'whatsapp-logo.svg',
+                title: 'WhatsApp',
+                desc: 'Social messenging service',
+                link: 'https://www.whatsapp.com/',
+            },
+            {
+                logo: 'discord-logo.svg',
+                title: 'Discord',
+                desc: 'VoIP and instant messanging platform',
+                link: 'https://discord.com/',
+            },
+            {
+                logo: 'kickstarter-logo.svg',
+                title: 'Kickstarter',
+                desc: 'Crowdfunding platform',
+                link: 'https://www.kickstarter.com/',
+            },
+            {
+                logo: 'hargreaves-lansdown-logo.svg',
+                title: 'Hargreaves Lansdown',
+                desc: 'Funds and shares financial service',
+                link: 'https://www.hl.co.uk/',
+            },
+        ],
+    },
 ]
--- src/components/TabCards/index.tsx
+++ src/components/TabCards/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { ConditionalWrapper } from '../ConditionalWrapper'
 import { Tabs } from './TabCardsTypes'
@@ -8,52 +8,71 @@ interface ITabCardsProps {
     tabs: Tabs[]
 }
 
-export const TabCards: React.FC<ITabCardsProps> = ({ title, tabs }) => (
-    <div className="xl:container mx-auto py-36">
-        <h2 className="font-sans text-white font-semibold text-center text-5xl max-w-4xl mx-auto mb-8">{title}</h2>
-        {tabs &&
-            tabs.map((tab, index) => (
-                <>
-                    <button className="font-sans text-white" key={index}>
-                        {tab.label}
-                    </button>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-7 justify-items-center max-w-4xl mx-auto">
-                        {tab.cards &&
-                            tab.cards.map((card, index) => (
-                                <div key={index} className="flex items-center max-w-[410px] md:max-w-none w-full">
-                                    <ConditionalWrapper
-                                        condition={!!card.link}
-                                        wrapper={(children) => (
-                                            <a
-                                                className="flex items-center h-full w-full bg-[#393939] p-4 sm:p-5 rounded-3xl lg:hover:-translate-y-2 transition hover:ease-in duration-200"
-                                                href={card.link}
-                                                target="_blank"
+export const TabCards: React.FC<ITabCardsProps> = ({ title, tabs }) => {
+    const [activeTab, setActiveTab] = useState<string>(tabs[0].label)
+
+    return (
+        <div className="xl:container mx-auto py-36">
+            <h2 className="font-sans text-white font-semibold text-center text-5xl max-w-4xl mx-auto mb-8">{title}</h2>
+            <div className="flex justify-center gap-3 mb-7">
+                {tabs &&
+                    tabs.map((tab) => (
+                        <button
+                            className="font-sans text-white"
+                            key={tab.label}
+                            onClick={() => setActiveTab(tab.label)}
+                        >
+                            {tab.label}
+                        </button>
+                    ))}
+            </div>
+            {tabs &&
+                tabs.map((tab, index) => (
+                    <>
+                        {activeTab === tab.label && (
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-7 justify-items-center max-w-4xl mx-auto">
+                                {tab.cards &&
+                                    tab.cards.map((card, index) => (
+                                        <div
+                                            data-label={tab.label}
+                                            key={index}
+                                            className="flex items-center max-w-[410px] md:max-w-none w-full"
+                                        >
+                                            <ConditionalWrapper
+                                                condition={!!card.link}
+                                                wrapper={(children) => (
+                                                    <a
+                                                        className="flex items-center h-full w-full bg-[#393939] p-4 sm:p-5 rounded-3xl lg:hover:-translate-y-2 transition hover:ease-in duration-200"
+                                                        href={card.link}
+                                                        target="_blank"
+                                                    >
+                                                        {children}
+                                                    </a>
+                                                )}
                                             >
-                                                {children}
-                                            </a>
-                                        )}
-                                    >
-                                        {card.logo && (
-                                            <div className="relative max-h-[28px] sm:max-h-[38px] max-w-[28px] sm:max-w-[38px] h-full w-full mr-4 sm:mr-7">
-                                                <Image
-                                                    src={`/logos/${card.logo}`}
-                                                    alt={`${card.title} logo`}
-                                                    layout="fill"
-                                                    objectFit="contain"
-                                                />
-                                            </div>
-                                        )}
-                                        <div>
-                                            <span className="block font-sans font-semibold text-lg text-white mb-1">
-                                                {card.title}
-                                            </span>
-                                            <p className="font-sans text-xs text-[#DBE2EF]">{card.desc}</p>
+                                                {card.logo && (
+                                                    <div className="relative max-h-[28px] sm:max-h-[38px] max-w-[28px] sm:max-w-[38px] h-full w-full mr-4 sm:mr-7">
+                                                        <Image
+                                                            src={`/logos/${card.logo}`}
+                                                            alt={`${card.title} logo`}
+                                                            layout="fill"
+                                                            objectFit="contain"
+                                                        />
+                                                    </div>
+                                                )}
+                                                <div>
+                                                    <span className="block font-sans font-semibold text-lg text-white mb-1">
+                                                        {card.title}
+                                                    </span>
+                                                    <p className="font-sans text-xs text-[#DBE2EF]">{card.desc}</p>
+                                                </div>
+                                            </ConditionalWrapper>
                                         </div>
-                                    </ConditionalWrapper>
-                                </div>
-                            ))}
-                    </div>
-                </>
-            ))}
-    </div>
-)
+                                    ))}
+                            </div>
+                        )}
+                    </>
+                ))}
+        </div>
+    )
+}
